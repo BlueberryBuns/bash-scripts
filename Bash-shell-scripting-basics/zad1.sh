ceb10bb3a39dfe63639625e35e75c645d212471c
HulewiczKamil <hulewicz.k@gmail.com>
2021-03-28 16:41:54 +0000

    Task 1.1

--- Bash-shell-scripting-basics/zad1.sh
+++ Bash-shell-scripting-basics/zad1.sh
@@ -21,5 +21,9 @@
 # Sprawdzić, czy w katalogu `ddd` istnieje plik o nazwie `pierwszy`,
 # a jeśli nie, to go utworzyć. Zawartość pliku nie ma znaczenia.
 #
+plik='./ddd/pierwszy'
 
-
+if [ ! -f "${plik}" ]; then
+    touch "${plik}"
+    echo "The file has been created"
+fi
