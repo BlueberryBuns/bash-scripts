6bcd2a751259b104c40fe3c32370b187a5b8a82e
HulewiczKamil <hulewicz.k@gmail.com>
2021-03-30 13:48:30 +0000

    Task1.7

--- Bash-shell-scripting-basics/zad7.sh
+++ Bash-shell-scripting-basics/zad7.sh
@@ -7,6 +7,7 @@
 # Utworzyć w katalogu `ddd` plik `ostrzeżenie`, w którym pojawi się informacja
 # "Uważaj na te pliki:" oraz lista znalezionych plików (każdy w nowej linii).
 #
+
 dir="bbb"
 file="ddd/ostrzeżenie"
 touch "${file}"
@@ -20,7 +21,7 @@ for element in "${1}"/*; do
         echo "$(basename ${element})" >> "${file}"
         echo "$(basename ${element})"
     fi
-done 
+done
 }
 
 find_executables "${dir}"
