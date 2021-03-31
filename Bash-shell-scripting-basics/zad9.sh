e8f9d5a770a0f6cbee75befd80d671021f117aa8
HulewiczKamil <hulewicz.k@gmail.com>
2021-03-31 11:29:29 +0000

    Task 1.9

--- Bash-shell-scripting-basics/zad9.sh
+++ Bash-shell-scripting-basics/zad9.sh
@@ -1,20 +1,4 @@
 #!/usr/bin/env bash
-#
-# Systemy operacyjne 2 – laboratorium nr 2 – semestr letni 2020/2021
-#
-# Celem zajęć jest nabranie doświadczenia w podstawowej pracy z powłoką Bash,
-# w szczególności w nawigowaniu po drzewie katalogów i sprawdzaniu uprawnień.
-#
-# Przygotowane rozwiązania nie muszą być całkowicie uniwersalne. Zakładamy,
-# że ogólna struktura katalogów się nie zmienia (nie będzie już więcej/mniej
-# poziomów podkatalogów), jednakże same nazwy i zawartości plików (o ile
-# nie są podane wprost w treści zadań) mogą być dowolne i ulegać zmianie,
-# a przygotowane rozwiązania nadal powinny działać.
-#
-# Wszystkie chwyty dozwolone, ale ostatecznie w wyniku ma powstać tylko to,
-# o czym mowa w treści zadania – tworzone samodzielnie ewentualne tymczasowe
-# pliki pomocnicze należy usunąć.
-#
 
 #
 # Zadanie 9.
@@ -27,4 +11,22 @@
 # że na pewno możemy je przeczytać).
 #
 
+secrets="ddd/zasoby/tajne hasła"
+src_directory="ccc"
+if [ -f "${secrets}" ]; then
+    rm "${secrets}"
+fi
+touch "${secrets}"
 
+con_files(){
+    for "${element}" in "${1}"/*; do
+        if [ -d "${element}" ]; then
+            con_files "${element}"
+        elif [ -f "${element}" -a -r "${element}" ]; then
+            echo "=== $(basename "${element}")" >> "${secrets}"
+            echo "$(cat "${element}")" >> "${secrets}"
+        fi
+    done
+}
+
+con_files "${src_dir}"
