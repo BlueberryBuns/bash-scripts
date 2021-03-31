61c9e625e85a512b30c8cb6673bfe1a5e0d78892
HulewiczKamil <hulewicz.k@gmail.com>
2021-03-31 18:14:22 +0000

    Task 1.10

--- Bash-shell-scripting-basics/zadA.sh
+++ Bash-shell-scripting-basics/zadA.sh
@@ -1,20 +1,4 @@
 #!/usr/bin/env bash
-#
-# Systemy operacyjne 2 – laboratorium nr 2 – semestr letni 2020/2021
-#
-# Celem zajęć jest nabranie doświadczenia w podstawowej pracy z powłoką Bash,
-# w szczególności w nawigowaniu po drzewie katalogów i sprawdzaniu uprawnień.
-#
-# Przygotowane rozwiązania nie muszą być całkowicie uniwersalne. Zakładamy,
-# że ogólna struktura katalogów się nie zmienia (nie będzie już więcej/mniej
-# poziomów podkatalogów), jednakże same nazwy i zawartości plików (o ile
-# nie są podane wprost w treści zadań) mogą być dowolne i ulegać zmianie,
-# a przygotowane rozwiązania nadal powinny działać.
-#
-# Wszystkie chwyty dozwolone, ale ostatecznie w wyniku ma powstać tylko to,
-# o czym mowa w treści zadania – tworzone samodzielnie ewentualne tymczasowe
-# pliki pomocnicze należy usunąć.
-#
 
 #
 # Zadanie 10.
@@ -32,4 +16,45 @@
 # źródłowych `nazwa-...`; każda nazwa to osobna linia w tych plikach).
 #
 
+array=("ddd/nazwy-1" "ddd/nazwy-2" "ddd/nazwy-3" "ddd/nazwy-4")
+lines=()
+clean_start=1
 
+foo(){
+    for filename in "${@}"; do
+        if [ -f ddd/zasoby/"${filename}" ]; then
+            echo $(($(cat ddd/zasoby/"${filename}")+1)) > ddd/zasoby/"${filename}"
+            echo ddd/zasoby/"${filename}" $(cat ddd/zasoby/"${filename}")
+        else
+            touch ddd/zasoby/"${filename}"
+            echo 0 > ddd/zasoby/"${filename}"
+        fi
+    done
+}
+
+del_foo(){
+    for filename in "${@}"; do
+        if [ -f ddd/zasoby/"${filename}" ]; then
+            rm ddd/zasoby/"${filename}"
+            echo "file ${filename} has been deleted"
+        fi
+    done
+}
+
+for file in "${array[@]}"; do
+    if [ -f "${file}" ]; then
+        IFS=$'\n' read -d '' -r -a lines < "${file}"
+        del_foo "${lines[@]}"
+    else
+        echo "Nie znaleziono pliku o nazwie ${file}"
+    fi
+done
+
+for file in "${array[@]}"; do
+    if [ -f "${file}" ]; then
+        IFS=$'\n' read -d '' -r -a lines < "${file}"
+        foo "${lines[@]}"
+    else
+        echo "Nie znaleziono pliku o nazwie ${file}"
+    fi
+done
