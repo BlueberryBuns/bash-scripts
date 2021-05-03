8c12edf0f75f2c86cc82d8a7827832dfafaa1ffb
HulewiczKamil <hulewicz.k@gmail.com>
2021-05-03 17:41:46 +0000

    Task 3.10

--- Bash-find/zadA.sh
+++ Bash-find/zadA.sh
@@ -1,22 +1,4 @@
 #!/usr/bin/env bash
-#
-# Systemy operacyjne 2 – laboratorium nr 4 – semestr letni 2020/2021
-#
-# Celem zajęć jest nabranie doświadczenia w pracy z programem `find`,
-# wykorzystując przy tym wiedzę na temat struktury systemu plików itd.
-# Większość rozwiązań powinno sprowadzić się do jednego, zmyślnego
-# wywołania programu `find` z odpowiednimi argumentami.
-#
-# Nie przywiązujemy wagi do środowiska roboczego – zakładamy, że jego pliki,
-# inne niż te podane wprost w treści zadań, mogą ulec zmianie, a przygotowane
-# rozwiązania nadal powinny działać poprawnie.
-#
-# Wszystkie chwyty dozwolone, ale ostatecznie w wyniku ma powstać tylko to,
-# o czym mowa w treści zadania – tworzone samodzielnie ewentualne tymczasowe
-# pliki pomocnicze należy usunąć.
-#
-# Punkt startowy powinien stanowić katalog ze źródłami: linux-5.11.13/
-#
 
 #
 # Zadanie 10.
@@ -29,3 +11,4 @@
 #
 
 
+find -L "linux-5.11.13" -name "mips" -prune -o \( -type f \( -name "core.c" -o -name "irq.c" \) -a \( \( -path "*bluetooth*" -a -size +30k \) -o \( -size 1k \) \) \) -printf "%p\n" #"%s %p\n"
