2aea886df3607acff7e05f1dcbbd5b7ac70e7011
HulewiczKamil <hulewicz.k@gmail.com>
2021-05-12 22:41:45 +0000

    Task 4.8

--- Pipelines/zad8.sh
+++ Pipelines/zad8.sh
@@ -1,19 +1,4 @@
 #!/usr/bin/env bash
-#
-# Systemy operacyjne 2 – laboratorium nr 5 – semestr letni 2020/2021
-#
-# Celem zajęć jest nabranie doświadczenia w pracy z mechanizmem łącz
-# nienazwanych, wykorzystując przy tym szereg podstawowych narzędzi
-# do przetwarzania danych tekstowych.
-#
-# Tradycyjnie nie przywiązujemy zbyt dużej wagi do środowiska roboczego.
-# Zakładamy, że format i układ danych w podanych plikach nie ulega zmianie,
-# ale same wartości, inne niż podane wprost w treści zadań, mogą ulec zmianie,
-# a przygotowane zadania wciąż powinny działać poprawnie (robić to, co trzeba).
-#
-# Wszystkie chwyty dozwolone, ale w wyniku ma powstać tylko to, o czym jest
-# mowa w treści zadania – nie generujemy żadnych dodatkowych plików pośrednich.
-#
 
 #
 # Zadanie 8.
@@ -23,3 +8,5 @@
 # Źródła znajdują się w katalogu `coreutils-8.32`.
 #
 
+find -P "coreutils-8.32" -type f | while read line; do grep -x "#include <stdio.h>" "${line}" | head -1 ; done |wc --lines
+#find -P "coreutils-8.32" -type f | grep "^(?!\s*$)(?:#include <stdio.h>| )+$-"  #while read line; do grep -x "#include <stdio.h>" "${line}"; done
