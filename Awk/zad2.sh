943fad39da898f74bd05c8747ceea097090cd30c
root <hulewicz.k@gmail.com>
2021-05-30 06:14:25 +0000

    Task 5.2

--- Awk/zad2.sh
+++ Awk/zad2.sh
@@ -1,20 +1,4 @@
 #!/usr/bin/env bash
-#
-# Systemy operacyjne 2 – laboratorium nr 6 – semestr letni 2020/2021
-#
-# Celem zajęć jest zapoznanie się z możliwościami narzędzia awk.
-#
-# Tradycyjnie nie przywiązujemy zbyt dużej wagi do środowiska roboczego.
-# Zakładamy, że format i układ danych w podanych plikach nie ulega zmianie,
-# ale same wartości, inne niż podane wprost w treści zadań, mogą ulec zmianie,
-# a przygotowane zadania wciąż powinny działać poprawnie (robić to, co trzeba).
-#
-# Wszystkie chwyty dozwolone, ale w wyniku ma powstać tylko to, o czym jest
-# mowa w treści zadania – nie generujemy żadnych dodatkowych plików pośrednich.
-# Większość rozwiązań powinno sprowadzić się do jednego, zmyślnego wywołania
-# programu `awk` z odpowiednimi argumentami.
-# 
-#
 
 #
 # Zadanie 2.
@@ -25,3 +9,8 @@
 # Każdą nazwę użytkownika wypisać w nowej linii.
 #
 
+awk '{
+split($0, line, ":")
+if (line[3] >= 1000 && line[7] == "/bin/bash")
+    print line[1]
+}' dodatkowe/etc-passwd
